5905afa927b5b831061092ca61a058c93ad59929
Mostafa Gamal <mostafa525.ma@gmail.com>
2023-01-14 21:08:47 +0100

    assignment 7 circular buffer implementation

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -29,10 +29,28 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
-    return NULL;
+    struct aesd_buffer_entry *return_buffer_entry = NULL;
+    uint8_t index = buffer->out_offs; 
+    struct aesd_buffer_entry *entryptr;
+    size_t current_offset = 0;
+    uint8_t count = 0;
+    //Start loop at index=out_offs and if max index reached and loop is still on then restart index at 0
+    //Using count as an isolated counter to end the loop if all elements are iterated
+    while(count < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED){
+        if(index >= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED){
+            index = 0;
+        }
+        entryptr=&((buffer)->entry[index]);
+        current_offset += entryptr->size;
+        if(current_offset > char_offset){
+            return_buffer_entry = entryptr;
+            *entry_offset_byte_rtn = char_offset - (current_offset - entryptr->size);
+            break;
+        }
+        index++;
+        count++;
+    }
+    return return_buffer_entry;
 }
 
 /**
@@ -44,9 +62,24 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    // Add a new entry at the value of in_offs
+    buffer->entry[buffer->in_offs] = *add_entry;
+    // Increment the value of in_offs
+    buffer->in_offs++;
+    // if the value reached the max already then restart at 0 again
+    if(buffer->in_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED){
+        buffer->in_offs = 0;
+    }
+    
+    //after in_offs has a new value in_offs == out_offs then the buffer is now full
+    if(buffer->in_offs == buffer->out_offs){
+        buffer->full = true;
+    }
+    //buffer was already full then we need to move out_offs to be equal in_offs
+    else if(buffer->full && buffer->in_offs > buffer->out_offs){
+        buffer->out_offs = buffer->in_offs;
+    }
+
 }
 
 /**
